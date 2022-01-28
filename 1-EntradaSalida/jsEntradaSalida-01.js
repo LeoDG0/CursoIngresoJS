4cfe557c92e99152a0ece6b4f2d150fb30b8fee6
leoDG0 <leonardodegregorio05@gmail.com>
2022-01-28 11:15:45 -0300

    estado normal x1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("hola mundo normal");
+	alert("hola mundo normal x1");
 }
 
